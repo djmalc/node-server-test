3bbe82afdefbb87f3075732e2ac786f8d0b668a4
David Malcolm <david@Kira-Mac.office.traverse.com.au>
2021-04-27 13:17:55 +1000

    refactor client code

--- Example-One/term-client.js
+++ Example-One/term-client.js
@@ -1,52 +1,58 @@
-
-import { XTerm } from 'xterm';
-import { AttachAddon } from 'xterm-addon-attach';
-import { FitAddon } from 'xterm-addon-fit';
+import { XTerm } from "xterm";
+import { AttachAddon } from "xterm-addon-attach";
+import { FitAddon } from "xterm-addon-fit";
 
 class ClientTerm {
-
+  constructor(opts) {
     const xTerm = new XTerm();
     const fitAddon = new FitAddon();
     const attachAddon = new AttachAddon();
-    xTerm.loadAddon('attach');
-    txTerm.loadAddon('fit');
+    xTerm.loadAddon("attach");
+    txTerm.loadAddon("fit");
 
     sendSizeToServer = () => {
-        let cols = this.term.cols.toString();
-        let rows = this.term.rows.toString();
-        while (cols.length < 3) {
-            cols = "0"+cols;
-        }
-        while (rows.length < 3) {
-            rows = "0"+rows;
-        }
-        this.socket.send("ESCAPED|-- RESIZE:"+cols+";"+rows);
+      let cols = this.term.cols.toString();
+      let rows = this.term.rows.toString();
+      while (cols.length < 3) {
+        cols = "0" + cols;
+      }
+      while (rows.length < 3) {
+        rows = "0" + rows;
+      }
+      this.socket.send("ESCAPED|-- RESIZE:" + cols + ";" + rows);
     };
 
     term = new xTerm({
-        cols: 80,
-        rows: 24
+      cols: 80,
+      rows: 24,
     });
     term.open(document.getElementById(opts.parentId), true);
 
     let sockHost = opts.host || "127.0.0.1";
     let sockPort = opts.port || 8080;
 
-    socket = new WebSocket("ws://"+sockHost+":"+sockPort);
+    socket = new WebSocket("ws://" + sockHost + ":" + sockPort);
     socket.onopen = () => {
-        term.attach(this.socket);
+      term.attach(this.socket);
     };
-    socket.onerror = (e) => {throw e};
+    socket.onerror = (e) => {
+      throw e;
+    };
 
     fit = () => {
-        term.fit();
-        setTimeout(() => {
-            sendSizeToServer();
-        }, 50);
-    }
+      term.fit();
+      setTimeout(() => {
+        sendSizeToServer();
+      }, 50);
+    };
 
     resize = (cols, rows) => {
-        term.resize(cols, rows);
-        sendSizeToServer();
-    }
+      term.resize(cols, rows);
+      sendSizeToServer();
+    };
+  }
 }
+
+module.exports = {
+    ClientTerm
+};
